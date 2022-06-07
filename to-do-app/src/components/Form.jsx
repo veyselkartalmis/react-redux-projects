6da71d010c6707c510d1e208bd1776e1d6a4ee62
Veysel Kartalmis <veyselkartalms@gmail.com>
2022-06-07 18:19:45 +0300

    prepare added

--- to-do-app/src/components/Form.jsx
+++ to-do-app/src/components/Form.jsx
@@ -1,30 +1,29 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
-import { nanoid } from "@reduxjs/toolkit";
-import { addTodo } from "../redux/todo/todoSlice"
+import { addTodo } from "../redux/todo/todoSlice";
 
 function Form() {
-    const [title, setTitle] = useState("");
-    const dispatch = useDispatch();
+	const [title, setTitle] = useState("");
+	const dispatch = useDispatch();
 
-    const handleSubmit = (e) => {
-        if (!title) return;
-        e.preventDefault();
-        dispatch(addTodo({ id: nanoid(), title, completed: false }));
-        setTitle("");
-    }
+	const handleSubmit = (e) => {
+		if (!title) return;
+		e.preventDefault();
+		dispatch(addTodo({ title }));
+		setTitle("");
+	};
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <input
-                className="new-todo"
-                placeholder="What needs to be done?"
-                autoFocus
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-            />
-        </form>
-    )
+	return (
+		<form onSubmit={handleSubmit}>
+			<input
+				className="new-todo"
+				placeholder="What needs to be done?"
+				autoFocus
+				value={title}
+				onChange={(e) => setTitle(e.target.value)}
+			/>
+		</form>
+	);
 }
 
 export default Form;
