660ff9308e4b126237daee26dcde7f22a99cdd1a
Veysel Kartalmis <veyselkartalms@gmail.com>
2022-06-14 18:46:51 +0300

    quotesSlice added

--- breaking-bad-app/src/redux/store.js
+++ breaking-bad-app/src/redux/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+
 import charactersSlice from "./charactersSlice";
+import quotessSlice from "./quotesSlice";
 
 export const store = configureStore({
     reducer: {
         characters: charactersSlice, 
+        quotes: quotessSlice, 
     },
 });
