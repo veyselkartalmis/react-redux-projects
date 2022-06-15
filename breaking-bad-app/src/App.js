6ac68f166e525ce5fc75625ba69ad0fdbec59e01
Veysel Kartalmis <veyselkartalms@gmail.com>
2022-06-15 18:59:56 +0300

    quotes detail page&route added

--- breaking-bad-app/src/App.js
+++ breaking-bad-app/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route, BrowserRouter, Link } from "react-router-dom";
 import Home from "./pages/Home/index";
 import Detail from "./pages/Detail/index";
 import Quotes from "./pages/Quotes/index";
+import QuotesDetail from "./pages/QuotesDetail/index";
 
 import "./index.css"
 
@@ -18,6 +19,7 @@ function App() {
           <Route exact path="/" element={<Home />}></Route>
           <Route path="/char/:char_id" element={<Detail />}></Route>
           <Route path="/quotes" element={<Quotes />}></Route>
+          <Route path="/quotes/:quote_id" element={<QuotesDetail />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
---
+++ breaking-bad-app/src/pages/QuotesDetail/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function QuotesDetail() {
+  return (
+    <div>QuotesDetail</div>
+  )
+}
+
+export default QuotesDetail;
