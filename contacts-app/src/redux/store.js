f770db19549df0add90e70ff42d95c3657c68f70
Veysel <veyselkartalms@gmail.com>
2022-06-18 23:42:18 +0300

    EntityAdapter added

--- contacts-app/src/redux/store.js
+++ contacts-app/src/redux/store.js
@@ -1,6 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import contactSlice from "./contactSlice";
 
 export default configureStore({
     reducer: {
+        contacts: contactSlice,
     }
 });
