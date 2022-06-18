b3f11ea6b60ef0478c796c9271b5039ff48c57f8
Veysel <veyselkartalms@gmail.com>
2022-06-18 18:48:41 +0300

    starter template

---
+++ spend-bill-gates-money/src/components/Card/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./card.scss";
+
+function Card() {
+  return (
+    
+    <div>Card</div>
+  )
+}
+
+
+export default Card
---
+++ spend-bill-gates-money/src/components/Receipt/Receipt.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./receipt.scss";
+
+function Receipt() {
+  return (
+    <div>Receipt</div>
+  )
+}
+
+export default Receipt
