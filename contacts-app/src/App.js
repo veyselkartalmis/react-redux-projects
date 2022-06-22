bdc33f5637326b939a097a1387b22d1ce69a5827
Veysel <veyselkartalms@gmail.com>
2022-06-22 21:36:43 +0300

    ..

--- contacts-app/src/App.js
+++ contacts-app/src/App.js
@@ -1,11 +1,10 @@
 import Contacts from "./components/Contacts/Index";
 
 function App() {
-	return (
-		<div className="App">
-			<Contacts />
-		</div>
-	);
+  return (
+    <div className="App">
+      <Contacts />
+    </div>
+  );
 }
-
 export default App;
