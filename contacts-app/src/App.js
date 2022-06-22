57273996efb66cc105f51d5906cea8808e54730e
Veysel <veyselkartalms@gmail.com>
2022-06-22 23:52:01 +0300

    router bug fixes

--- contacts-app/src/App.js
+++ contacts-app/src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route, BrowserRouter, } from "react-router-dom";
 import Contacts from "./components/Contacts/index";
 import Edit from "./components/Contacts/Edit";
+import Error from "./components/Error";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
           <Routes>
             <Route exact path="/" element={<Contacts />}></Route>
             <Route path="/edit/:id" element={<Edit />}></Route>
+            <Route path="*" element={<Error />}></Route>
           </Routes>
         </BrowserRouter>
       </div>
